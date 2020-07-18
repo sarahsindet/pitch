b7e4d2df4eb800b89dcf0ba5a9610976ed35d049
sarahsindet <sarahsindet@gmail.com>
2020-07-19 00:48:16 +0300

    added a sqldb

--- pitch.py
+++ pitch.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, url_for,flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sindet'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
