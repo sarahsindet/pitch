b0245107656c1c266071309d9f3f54ad32aea2fb
sarahsindet <sarahsindet@gmail.com>
2020-07-18 23:36:50 +0300

    imported sql

--- pitch.py
+++ pitch.py
@@ -1,5 +1,6 @@
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, url_for,flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sindet'
