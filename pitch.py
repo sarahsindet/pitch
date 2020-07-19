e8f605abcd9c99dc2a1e806aaa8b6c2788150108
sarahsindet <sarahsindet@gmail.com>
2020-07-20 00:05:43 +0300

    imported user and posts fromthe models

--- pitch.py
+++ pitch.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, url_for,flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from models import User, Posts
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sindet'
